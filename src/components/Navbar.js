1cd10e3c098411b5aeb59b340edfcb330de6ed44
kaml89 <rba.kamil@gmail.com>
2020-09-01 22:34:47 +0200

    edit Navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,21 @@
 import React from 'react'
+import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
 
-const Navbar = () => {
+const Navbar = ({ loggedUser }) => {
     return (
         <div>
             <Link to={'/blogs'}>Blogs</Link>
             <Link to={'/users'}>Users</Link>
+            <span>{ loggedUser ? `${loggedUser.username} is logged in` : '' }</span>
         </div>
     )
 }
 
-export default Navbar
+const mapStateToProps = state => {
+    return {
+        loggedUser: state.auth.user
+    }
+}
+
+export default connect(mapStateToProps)(Navbar)
